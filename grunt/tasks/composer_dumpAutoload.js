5b08c152655eacd1c5869d7d98e13873288c518e
jhotadhari <tellme@waterproof-webdesign.info>
2020-08-21 11:28:47 +0530

    Change task composer_dumpAutoload
    Adjust composer/autoload_static.php classMap
    Add function to remove all entries for `croox/wp-dev-env-frame`, if plugin active `Croox__wp-dev-env-frame-loader_wp-plugin`

--- grunt/tasks/composer_dumpAutoload.js
+++ grunt/tasks/composer_dumpAutoload.js
@@ -1,4 +1,5 @@
 
+const path = require( 'path' );
 const chalk = require( 'chalk' );
 
 const composer_dumpAutoload = grunt => {
@@ -12,13 +13,32 @@ const composer_dumpAutoload = grunt => {
 			args: [
 				'dump-autoload',
 				'--optimize',
+				'--classmap-authoritative',
 			],
 		}, ( error, result, code ) => {
+			// Maybe throw error
 			if ( error ) {
 				grunt.warn( error );
 				done.apply( false );
 			}
 
+			// Adjust composer/autoload_static.php classMap
+			// Add function to remove all entries for `croox/wp-dev-env-frame`, if plugin active `Croox__wp-dev-env-frame-loader_wp-plugin`
+			const autoload_static_path = path.resolve( 'vendor/composer/autoload_static.php' )
+			grunt.file.write( autoload_static_path, grunt.file.read( autoload_static_path ).replace( /^(\s*?)\$loader->classMap\s*?=\s*?(ComposerStaticInit[a-z\d]+?::\$classMap);/gm, ( match, p1, p2 ) => [
+				p1 + 'if ( function_exists( "is_plugin_active" )',
+				p1 + '  && false === strpos( __DIR__, "Croox__wp-dev-env-frame-loader_wp-plugin" )',
+				p1 + '	&& is_plugin_active( "Croox__wp-dev-env-frame-loader_wp-plugin/Croox__wp-dev-env-frame-loader_wp-plugin.php" )',
+				p1 + ') {',
+				p1 + '	$loader->classMap = array_filter( ' + p2 + ', function( $path ) {',
+				p1 + '		return false === strpos( $path, "croox/wp-dev-env-frame" );',
+				p1 + '	} );',
+				p1 + '} else {',
+				p1 + '	$loader->classMap = ' + p2 + ';',
+				p1 + '}',
+			].join( '\n' ) );
+
+			// Print result
 			[
 				 result.stdout,
 				 result.stderr,
@@ -32,3 +52,4 @@ const composer_dumpAutoload = grunt => {
 };
 
 module.exports = composer_dumpAutoload;
+
